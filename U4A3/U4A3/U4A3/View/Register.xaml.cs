6a285fff41d1b3a8bb2b14b1fefca2f35a031044
Cameron Stuart <cameronstuart861@gmail.com>
2018-07-16 19:56:04 +0100

    Registration page improvements Completed registration page and added registration functionality. Started work on login functionality
    TODO:
    Test if database works
    Create login functionality

--- U4A3/U4A3/U4A3/View/Register.xaml.cs
+++ U4A3/U4A3/U4A3/View/Register.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+
+using U4A3.Data;
+using U4A3.Model;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +17,49 @@ namespace U4A3.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Register : ContentPage
 	{
+		StringBuilder ErrorBuilder = new StringBuilder();
+
 		public Register ()
 		{
 			InitializeComponent ();
+		}
+
+		private async void Button_Register_Clicked(object sender, EventArgs e)
+		{
+			if (Entry_Name.Text != ""
+				&& Entry_Username.Text != ""
+				&& Entry_Password.Text != ""
+				&& Entry_Email.Text != ""
+				&& Picker_Type.SelectedIndex != -1)
+			{
+				User user = new User()
+				{
+					Name = Entry_Name.Text,
+					Username = Entry_Username.Text,
+					Email = Entry_Email.Text,
+					Password = Entry_Password.Text,
+					Type = Picker_Type.Items[Picker_Type.SelectedIndex]
+				};
+
+				await App.Database.Insert(user);
+
+				App.Current.MainPage = new Login();
+			}
+			else
+			{
+				if (Entry_Name.Text == "")
+					Label_NameError.Text = "Please enter your name";
+				if (Entry_Username.Text == "")
+					Label_UsernameError.Text = "Please enter a username";
+				if (Entry_Password.Text == "")
+					Label_PasswordError.Text = "Please enter a password";
+				if (Entry_Email.Text == "")
+					Label_EmailError.Text = "Please enter your email";
+				if (Entry_Email.Text != "" && Regex.IsMatch(Entry_Email.Text, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))
+					Label_EmailError.Text = "Please enter a valid email";
+				if (Picker_Type.SelectedIndex == -1)
+					Label_TypeError.Text = "Please select an account type";
+			}
 		}
 	}
 }
